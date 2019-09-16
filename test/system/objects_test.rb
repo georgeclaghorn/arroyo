53e40e7ccbd44fd502547f08a636627a7cc4afea
George Claghorn <george@basecamp.com>
2019-09-15 23:17:49 -0400

    Use random object keys in system tests
    Avoid spurious interaction between concurrent test runs.

--- test/system/objects_test.rb
+++ test/system/objects_test.rb
@@ -1,30 +1,31 @@
 require "system_test_helper"
 
 class Arroyo::ObjectsSystemTest < ActiveSupport::TestCase
-  teardown { @bucket.delete "file.txt" }
+  setup { @key = SecureRandom.base58(24) }
+  teardown { @bucket.delete @key }
 
   test "uploading from and downloading to a path on disk" do
-    Tempfile.create([ "up", ".txt" ]) do |file|
+    Tempfile.create("up") do |file|
       file.write "Hello world!"
       file.flush
-      @bucket.upload "file.txt", file.path
+      @bucket.upload @key, file.path
     end
 
-    Tempfile.create([ "down", ".txt" ]) do |file|
-      @bucket.download "file.txt", file.path
+    Tempfile.create("down") do |file|
+      @bucket.download @key, file.path
       assert_equal "Hello world!", file.read
     end
   end
 
   test "uploading from and downloading to a file" do
-    Tempfile.create([ "up", ".txt" ]) do |file|
+    Tempfile.create("up") do |file|
       file.write "Hello world!"
       file.rewind
-      @bucket.upload "file.txt", file
+      @bucket.upload @key, file
     end
 
-    Tempfile.create([ "down", ".txt" ]) do |file|
-      @bucket.download "file.txt", file
+    Tempfile.create("down") do |file|
+      @bucket.download @key, file
 
       file.rewind
       assert_equal "Hello world!", file.read
@@ -32,14 +33,14 @@ class Arroyo::ObjectsSystemTest < ActiveSupport::TestCase
   end
 
   test "checksumming" do
-    Tempfile.create([ "up", ".txt" ]) do |file|
+    Tempfile.create("up") do |file|
       file.write "Hello world!"
       file.rewind
-      @bucket.upload "file.txt", file
+      @bucket.upload @key, file
     end
 
     checksum = Digest::SHA256.new.tap do |digest|
-      @bucket.download("file.txt") do |io|
+      @bucket.download(@key) do |io|
         io.each { |chunk| digest << chunk }
       end
     end.base64digest
@@ -48,10 +49,10 @@ class Arroyo::ObjectsSystemTest < ActiveSupport::TestCase
   end
 
   test "uploading and downloading binary data" do
-    @bucket.upload "file.txt", file_fixture("daisy.jpg").open
+    @bucket.upload @key, file_fixture("daisy.jpg").open
 
     checksum = Digest::SHA256.new.tap do |digest|
-      @bucket.download("file.txt") do |io|
+      @bucket.download(@key) do |io|
         io.each { |chunk| digest << chunk }
       end
     end.base64digest
@@ -60,16 +61,14 @@ class Arroyo::ObjectsSystemTest < ActiveSupport::TestCase
   end
 
   test "uploading from a large file in multiple parts" do
-    @bucket.upload "starry_night.jpg", file_fixture("starry_night.jpg").open
+    @bucket.upload @key, file_fixture("starry_night.jpg").open
 
     checksum = Digest::SHA256.new.tap do |digest|
-      @bucket.download("starry_night.jpg") do |io|
+      @bucket.download(@key) do |io|
         io.each { |chunk| digest << chunk }
       end
     end.base64digest
 
     assert_equal Digest::SHA256.file(file_fixture("starry_night.jpg")).base64digest, checksum
-  ensure
-    @bucket.delete "starry_night.jpg"
   end
 end
